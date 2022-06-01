e3a0acbf6ff0b465510643c5bb06e2ab5a869fc3
hqa 3 <hqa3@cdc.gov>
2022-06-01 13:47:42 +0700

    change criteria

--- Code/flowchart.R
+++ Code/flowchart.R
@@ -3,8 +3,8 @@ library(DiagrammeRsvg)
 library(DiagrammeR)
 
 df <- tblpart1 %>% 
-#  filter(Hospital=='Nakorn Phanom') %>%
-#  filter(Hospital=='Mae Sot') %>% 
+# filter(Hospital=='Nakorn Phanom') %>%
+# filter(Hospital=='Mae Sot') %>% 
   summarise(scr = n(),
             inf = sum(IFTropSep == TRUE | 
                         IFRespiratory == TRUE |
@@ -17,36 +17,35 @@ df <- tblpart1 %>%
             exc = sum(Refer48hrs == 1, na.rm = TRUE),
             sirs = sum(SirsTemp + SirsHR + SirsRR + SirsWBC >= 2, na.rm = TRUE),
             sofa = sum(qS_RR + qS_BP + qS_Glasgow >= 2, na.rm = TRUE),
-            sussep = sum(SirsTemp + SirsHR + SirsRR + SirsWBC >= 2 |
-                        qS_RR + qS_BP + qS_Glasgow >= 2, na.rm = TRUE),
+            sussep = sum(SirsTemp + SirsHR + SirsRR + SirsWBC >= 2, na.rm = TRUE),
             enr = sum(!is.na(SepsisID), na.rm = TRUE))
 
 df2 <- psmast %>%
-#  filter(HospitalID ==1) %>%
-#  filter(HospitalID ==2) %>%
-  mutate(Sepsis_Scale = ifelse(Sepsis_Scale == 1 &
-                               (SepReview1 %in% c(1,2,3) | SepReview2 %in% c(1,2,3)),2,Sepsis_Scale)) %>% 
-  summarise(nonsepsis = sum(Sepsis_Scale == 1, na.rm = TRUE),
-            sepsis    = sum(Sepsis_Scale == 2, na.rm = TRUE),
-            shock     = sum(Sepsis_Scale == 3, na.rm = TRUE),
-            pending   = sum(is.na(Sepsis_Scale), na.rm = TRUE))
+# filter(HospitalID ==1) %>%
+ #filter(HospitalID ==2) %>%
+  
+  summarise(nonsepsis = sum(Sofa_Sepsis == 2, na.rm = TRUE),
+            sepsis    = sum(Sofa_Sepsis == 1, na.rm = TRUE),
+            complete   = sum(!is.na(DischargeStatus) &!is.na(Sofa_Sepsis), na.rm = TRUE),
+            CompleteSepsis    = sum(!is.na(DischargeStatus) & Sofa_Sepsis == 1, na.rm = TRUE),
+            CompleteNonsepsis = sum(!is.na(DischargeStatus) & Sofa_Sepsis == 2, na.rm = TRUE))
   
 l1 <- paste0(scales::comma(df$scr),' persons age >= 15 years visit at ER/OPD', '\n', ' were screened')
 l2 <- paste0(df$inf, ' (', scales::percent(df$inf/df$scr,0.1), ') with suspected infection')
 l3 <- paste0(df$exc, ' (', scales::percent(df$exc/df$scr,0.1), ') Admitted > 48 hours','\n', 'from referral hospital')
-l4 <- paste0(df$sussep, ' (', scales::percent(df$sussep/df$inf,0.1), ') suspected sepsis (SIRS or qSOFA >= 2)', '\n',
-             df$sirs, ' (', scales::percent(df$sirs/df$inf,0.1), ') SIRS >= 2', '\n',
-             df$sofa, ' (', scales::percent(df$sofa/df$inf,0.1), ') SOFA >= 2')
+l4 <- paste0(df$sussep, ' (', scales::percent(df$sussep/df$inf,0.1), ') suspected sepsis (SIRS >= 2)', '\n',
+             df$sirs, ' (', scales::percent(df$sirs/df$inf,0.1), ') SIRS >= 2')
 l5 <- paste0(df$enr, ' (', scales::percent(df$enr/df$sussep,0.1), ') enrolled suspected sepsis')
-l6 <- paste0(df2$nonsepsis, ' (', scales::percent(df2$nonsepsis/df$enr,0.1), ') non-sepsis')
-l7 <- paste0(df2$sepsis, ' (', scales::percent(df2$sepsis/df$enr,0.1), ') sepsis','\n','(SOFA >= 2 or doctor determine)')
-l8 <- paste0(df2$shock, ' (', scales::percent(df2$shock/df$enr,0.1), ') septic shock','\n','(MAP <65 mmHg)')
-# l9 <- paste0(df2$pending, ' (', scales::percent(df2$pending/df$enr,0.1), ') pending')
+l6 <- paste0(df2$complete, ' (', scales::percent(df2$complete/df$enr,0.1), ') with complete data','\n','(having discharge status)')
+l8 <- paste0(df2$CompleteNonsepsis, ' (', scales::percent(df2$CompleteNonsepsis/df2$complete,0.1), ') non-sepsis')
+l7 <- paste0(df2$CompleteSepsis, ' (', scales::percent(df2$CompleteSepsis/df2$complete,0.1), ') sepsis','\n','(SOFA >= 2)')
+#l8 <- paste0(df2$shock, ' (', scales::percent(df2$shock/df$enr,0.1), ') septic shock','\n','(MAP <=65 mmHg and lactate > 2.0)')
+#l8 <- paste0(df2$pending, ' (', scales::percent(df2$pending/df$enr,0.1), ') pending')
 
 DiagrammeR::grViz("digraph graphtest {
   
   graph [layout = dot,
-         label = 'ALL',
+         label = 'All',
          labelloc = t,
          fontname = Helvetica,
          fontsize  = 36]
@@ -60,9 +59,10 @@ DiagrammeR::grViz("digraph graphtest {
   infection [label = '@@2']
   s_sepsis [label = '@@4']
   enrolled [label = '@@5']
-  nonsepsis [label = '@@6', width = 4]
-  sepsis [label = '@@7', width = 4]
-  shock [label = '@@8', width = 4]
+  complete [label = '@@6', width = 4]
+  CompleteSepsis [label = '@@7', width = 4]
+  CompleteNonsepsis [label = '@@8', width = 4]
+ 
   blank [label = '', width = 0.01, height = 0.01]
   excluded [label = '@@3']
   
@@ -72,9 +72,10 @@ DiagrammeR::grViz("digraph graphtest {
   blank -> excluded[ minlen = 5 ];
   blank -> infection;
   infection -> s_sepsis -> enrolled;
-  enrolled -> nonsepsis;
-  enrolled -> sepsis;
-  enrolled -> shock
+  enrolled -> complete;
+  complete -> CompleteSepsis;
+  complete -> CompleteNonsepsis;
+ 
 }
   [1]: l1
   [2]: l2
@@ -84,5 +85,6 @@ DiagrammeR::grViz("digraph graphtest {
   [6]: l6
   [7]: l7
   [8]: l8
+  
 ") 
 
